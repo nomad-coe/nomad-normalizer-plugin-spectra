95e885fe74aec6b6ef57c255d5eacee1e2728a42
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2024-01-25 03:16:13 +0100

    Remove unnecessary dependencies

--- spectranormalizer/normalizer.py
+++ spectranormalizer/normalizer.py
@@ -16,23 +16,23 @@
 # limitations under the License.
 #
 
-from runschema.calculation import Spectra, ElectronicStructureProvenance
 from nomad.normalizing.normalizer import Normalizer
 
 
 class SpectraNormalizer(Normalizer):
     """Normalizer for Spectra in run.calculation with the following responsibilities:
 
-      - Sets provenance method section.
-      - Normalizes intensities to their maximum value.
+    - Sets provenance method section.
+    - Normalizes intensities to their maximum value.
     """
+
     def normalize(self, logger=None) -> None:
         # Setup logger
         if logger is not None:
             self.logger = logger.bind(normalizer=self.__class__.__name__)
 
         # SinglePoint
-        if self.entry_archive.m_xpath('run[-1].calculation[-1].spectra'):
+        if self.entry_archive.m_xpath("run[-1].calculation[-1].spectra"):
             calc_section = self.entry_archive.run[-1].calculation[-1]
             for spectra in calc_section.spectra:
                 if self.is_valid_spectra(spectra):
@@ -40,16 +40,20 @@ class SpectraNormalizer(Normalizer):
                     if not calc_section.method_ref:
                         return
                     if calc_section.method_ref.photon and not spectra.provenance:
-                        provenance = ElectronicStructureProvenance(methodology=calc_section.method_ref, label='photon')
-                        spectra.m_add_sub_section(Spectra.provenance, provenance)
+                        provenance_cls = spectra.m_def.all_sub_sections[
+                            "provenance"
+                        ].sub_section.section_cls
+                        provenance = provenance_cls(
+                            methodology=calc_section.method_ref, label="photon"
+                        )
+                        spectra.provenance.append(provenance)
                     # Normalizing intensities to their maximum value.
                     # spectra.intensities = spectra.intensities / max(spectra.intensities)
                     # TODO set same excitations_energies reference for OCEAN (BSE), exciting (BSE),
                     # and XSPECTRA (Fermi rule) codes. Generalize for others
 
-    def is_valid_spectra(self, spectra: Spectra) -> bool:
-        """Used to check that a spectra has all required information for normalization.
-        """
+    def is_valid_spectra(self, spectra) -> bool:
+        """Used to check that a spectra has all required information for normalization."""
         if spectra.excitation_energies is not None and spectra.intensities is not None:
             n_energies = len(spectra.excitation_energies)
             n_intensities = len(spectra.intensities)
@@ -57,13 +61,17 @@ class SpectraNormalizer(Normalizer):
                 spectra.n_energies = n_energies
             else:
                 self.logger.warning(
-                    'Empty arrays or size of arrays do not coincide: could not validate spectra.')
+                    "Empty arrays or size of arrays do not coincide: could not validate spectra."
+                )
                 return False
             if (spectra.intensities < 0.0).any():
-                self.logger.warning('Invalid negative intensities found: could not validate spectra.')
+                self.logger.warning(
+                    "Invalid negative intensities found: could not validate spectra."
+                )
                 return False
             return True
         else:
             self.logger.warning(
-                'Parsed spectra do not contain excitation_energies and intensitites to be normalized.')
+                "Parsed spectra do not contain excitation_energies and intensitites to be normalized."
+            )
             return False
